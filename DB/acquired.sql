4b8af897ba0f955b35323ffeb7eacc84bb567c95
Jaden <jadedsyiem@gmail.com>
2025-08-01 12:42:25 +0530

    Create acquired.sql

---
+++ DB/acquired.sql
@@ -0,0 +1,7 @@
+CREATE TABLE acquired (serial_no INTEGER NOT NULL,
+  date DATE NOT NULL,
+  eng_received_from VARCHAR (255) NOT NULL,
+  hi_received_from VARCHAR (255) NOT NULL,
+  letter_no VARCHAR (255) NOT NULL,
+  eng_subject VARCHAR (255) NOT NULL,
+  hi_subject VARCHAR (255) NOT NULL);
